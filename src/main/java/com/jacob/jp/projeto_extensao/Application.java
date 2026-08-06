4a43b3275b352a3a72882e6605a5fef69399321d
AlsornaJP <jpribeirojacob@gmail.com>
2026-08-06 20:05:43 -0300

    chore: setup inicial do projeto
     Adiciona estrutura base do projeto (Maven wrapper, pom.xml e
     configurações do repositório), sem lógica de negócio ainda.

---
+++ src/main/java/com/jacob/jp/projeto_extensao/Application.java
@@ -0,0 +1,13 @@
+package com.jacob.jp.projeto_extensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacob/jp/projeto_extensao/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.jp.projeto_extensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
